c62c91fb04277116f0a34a11ace6febd7992b1e4
CraciDenisStefan <crdenis13@gmail.com>
2023-07-31 14:30:27 +0200

    Creato le colonne

--- database/migrations/2023_07_31_115935_create_comics_table.php
+++ database/migrations/2023_07_31_115935_create_comics_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb')->nullable();
+            $table->string('cover_image')->nullable();
+            $table->text('thumb2')->nullable();
+            $table->decimal('price', 5,2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type')->nullable();
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
+
             $table->timestamps();
         });
     }
