e33f4a5f28378d2906c78aacb31c16760875d11e
CraciDenisStefan <crdenis13@gmail.com>
2023-07-31 18:11:08 +0200

    seeder ok

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -14,6 +16,27 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $array_comics = config('comic');
+
+        foreach($array_comics as $item){
+
+            $comic = new Comic();
+
+            $artists = implode(', ', $item['artists']);
+            $writers = implode(', ', $item['writers']);
+
+            $comic->title = $item['title'];
+            $comic->description = $item['description'];
+            $comic->thumb = $item['thumb'];
+            $comic->cover_image = $item['cover_image'];
+            $comic->price = $item['price'];
+            $comic->series = $item['series'];
+            $comic->sale_date = $item['sale_date'];
+            $comic->artists = $artists;
+            $comic->writers = $writers;
+
+            $comic->save();
+
+        }
     }
 }
